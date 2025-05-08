39d95d71721e53551a52fb019765e6f38298a2a8
zahrag <zahra.gharaee@gmail.com>
2025-05-08 12:56:28 -0400

    up vis

--- visualization.py
+++ visualization.py
@@ -4,44 +4,13 @@ from utils import *
 from plot_functions import *
 from barcode_pwd_spark import BarcodePWD
 from tqdm import tqdm
+import matplotlib.colors as mcolors
 
 
-def agg_bin_groups(df, num_bins=100):
+def _plt_sdi(args, ranked_data, _plt=False):
 
-    # Get a df where each entry is a unique group of df (under 'group_name'), and its mean distance (under 'distance')
-    mean_distances = df.groupby('group_name')['distance'].mean().reset_index()
-
-    # Rename the columns for clarity: 'distance' changed to 'mean_distance'
-    mean_distances.columns = ['group_name', 'mean_distance']
-
-    # Rank the groups based on mean distance descending (sorting them so that larger numbers come first, followed by smaller numbers)
-    mean_distances['rank'] = mean_distances['mean_distance'].rank(method='first', ascending=False)
-
-    # Sort by rank for better readability
-    mean_distances = mean_distances.sort_values(by='rank')
-
-    mean_distances['bins'] = pd.qcut(mean_distances['rank'], q=num_bins,
-                                     labels=[f'Bin {i + 1}' for i in range(num_bins)])
-
-    # Calculate mean distances for each bin
-    bin_means = mean_distances.groupby('bins')['mean_distance'].mean()
-
-    plt.figure(figsize=(10, 6))
-    mean_distances.groupby('bins')['mean_distance'].mean().plot(kind='bar')  # Fix here
-    plt.title('Mean Distances by Bins')
-    plt.xlabel('Bins')
-    plt.ylabel('Mean Distance')
-    # Set x-ticks to show the mean distances instead of Bin 1, Bin 2, ...
-    plt.xticks(ticks=range(len(bin_means)), labels=[f'{mean:.2f}' for mean in bin_means], rotation=90, fontsize=7)
-    plt.tight_layout()
-    plt.show()
-
-    return mean_distances
-
-
-def main(args):
-
-    ranked_data = open_pickle(pickle_file=args.data_hierarchy_file)
+    if not _plt:
+        return
 
     # Example: flatten data_hierarchy into a DataFrame
     rows = []
@@ -61,6 +30,12 @@ def main(args):
         "genus": "#8c564b",
         "species": "#e377c2",
     }
+    weights = [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3]
+    colors = plt.cm.Reds(weights)
+    color_hex = [mcolors.to_hex(color) for color in colors]
+    ranks = ["phylum", "class", "order", "family", "subfamily", "genus", "species"]
+    color_dict = dict(zip(ranks, color_hex))
+
     custom_order = list(color_dict.keys())
 
     fig_file = os.path.join(args.save_path, f'sdi_distributions.pdf')
@@ -73,18 +48,9 @@ def main(args):
              x_label='Taxonomic Ranks',
              y_label='Shannon Diversity Index',
              tick_font_size=24,
-             _plt=False)
+             _plt=_plt)
 
-    for rank, groups in ranked_data.items():
-        if rank != args.rank:
-            continue
-        sorted_group_names = sorted(
-            groups.keys(),
-            key=lambda g: groups[g]['sdi'],
-            reverse=True
-        )
-        sorted_groups_by_sdi = sorted_group_names
-        sorted_groups_alphabetically = sorted(sorted_group_names)  # or names.sort(reverse=True)
+def load_distances(args, _to_panda=False):
 
     pwd = BarcodePWD()
     distances_dir = os.path.join(args.distances_path, args.rank)
@@ -96,69 +62,95 @@ def main(args):
         df = pwd._load_from_parquet(distances_dir_chk)
         distances = df if distances is None else distances.union(df)
 
-    # distances = load_from_pandas(args.distances_file, load_file=True)
     # Convert Spark DataFrame to Pandas if necessary
-    distances = distances.toPandas()
+    distances_pandas = None
+    if _to_panda:
+        distances_pandas = distances.toPandas()
 
-    # for rank, subgroups in ranked_data.items():
-    #     if rank != args.rank:
-    #         continue
-    #     for subgroup_name, subgroup_data in subgroups.items():
-    #         n_barcodes = len(subgroup_data['barcodes'])
-    #         if n_barcodes < 5:
-    #             continue
-    #         # Expected number of comparisons
-    #         n = 1000 if len(subgroup_data['barcodes']) > 1000 else len(subgroup_data['barcodes'])
-    #         expected = n * (n - 1) // 2
-    #
-    #         # Actual number of comparisons in your DataFrame
-    #         if isinstance(distances, pd.DataFrame):
-    #             actual = distances[distances['subgroup_name'] == subgroup_name].shape[0]
-    #         else:
-    #             actual = distances.filter(distances['subgroup_name'] == subgroup_name).count()
-    #
-    #         print(f"[{rank}::{subgroup_name}] expected={expected}, actual={actual}")
-    #         if expected == actual:
-    #             print("  ✅ Match")
-    #         else:
-    #             print("  ❌ Mismatch")
+    return distances, distances_pandas
 
-    metadata = load_from_pandas(args.metadata_file, load_file=True)
+def check_match_mismatch(args, distances, ranked_data, _check=False):
 
-    # Define a list of 10 colors
-    color_palette = [
-        "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-        "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
-    ]
+    if not _check:
+        return
 
-    # Get unique classes from the 'class' column
-    unique_classes = metadata['class'].dropna().unique()
+    for rank, subgroups in ranked_data.items():
+        if rank != args.rank:
+            continue
+        for subgroup_name, subgroup_data in subgroups.items():
+            n_barcodes = len(subgroup_data['barcodes'])
+            if n_barcodes < 5:
+                continue
+            # Expected number of comparisons
+            n = 1000 if len(subgroup_data['barcodes']) > 1000 else len(subgroup_data['barcodes'])
+            expected = n * (n - 1) // 2
 
-    # Create a dictionary mapping each class to a unique color
-    class_color_map = {class_name: color_palette[i] for i, class_name in enumerate(unique_classes)}
+            # Actual number of comparisons in your DataFrame
+            if isinstance(distances, pd.DataFrame):
+                actual = distances[distances['subgroup_name'] == subgroup_name].shape[0]
+            else:
+                actual = distances.filter(distances['subgroup_name'] == subgroup_name).count()
 
-    # Filter the DataFrame to exclude rows with NaN in 'class' or 'order'
-    filtered_df = metadata.dropna(subset=['class', 'order'])
+            print(f"[{rank}::{subgroup_name}] expected={expected}, actual={actual}")
+            if expected == actual:
+                print("  ✅ Match")
+            else:
+                print("  ❌ Mismatch")
 
-    # Map colors to each order based on its class
-    order_color_map = {order: class_color_map[class_name]
-                        for order, class_name in zip(filtered_df['order'], filtered_df['class'])}
 
-    # Map order to their corresponding classes
-    order_class_map = {order: class_name for order, class_name in zip(filtered_df['order'], filtered_df['class'])}
-    print(order_class_map)
+def agg_bin_subgroups(df, num_bins=100, _plt=False):
 
-    if args.rank == 'class':
-        color_dict = class_color_map
-    elif args.rank == 'order':
-        color_dict = order_color_map
+    # Get a df where each entry is a unique group of df (under 'group_name'), and its mean distance (under 'distance')
+    mean_distances = df.groupby('subgroup_name')['distance'].mean().reset_index()
+
+    # Rename the columns for clarity: 'distance' changed to 'mean_distance'
+    mean_distances.columns = ['subgroup_name', 'mean_distance']
+
+    # Rank the groups based on mean distance descending (sorting them so that larger numbers come first, followed by smaller numbers)
+    mean_distances['rank'] = mean_distances['mean_distance'].rank(method='first', ascending=False)
+
+    # Sort by rank for better readability
+    mean_distances = mean_distances.sort_values(by='rank')
+
+    mean_distances['bins'] = pd.qcut(mean_distances['rank'], q=num_bins,
+                                     labels=[f'Bin {i + 1}' for i in range(num_bins)])
+
+    # Calculate mean distances for each bin
+    bin_means = mean_distances.groupby('bins')['mean_distance'].mean()
+
+    if _plt:
+        plt.figure(figsize=(10, 6))
+        mean_distances.groupby('bins')['mean_distance'].mean().plot(kind='bar')  # Fix here
+        plt.title('Mean Distances by Bins')
+        plt.xlabel('Bins')
+        plt.ylabel('Mean Distance')
+        # Set x-ticks to show the mean distances instead of Bin 1, Bin 2, ...
+        plt.xticks(ticks=range(len(bin_means)), labels=[f'{mean:.2f}' for mean in bin_means], rotation=90, fontsize=7)
+        plt.tight_layout()
+        plt.show()
+
+    return mean_distances
+
+
+def main(args):
+
+    ranked_data = open_pickle(pickle_file=args.data_hierarchy_file)
+
+    # Plot SDI distributions Across 7 Taxonomic Ranks
+    _plt_sdi(args, ranked_data, _plt=False)
+
+    # Load pairwise distances of DNA barcode from parquets
+    distances, distances_pandas = load_distances(args, _to_panda=True)
+
+    # Sanity checks of the number of distances
+    check_match_mismatch(args, distances, ranked_data, _check=False)
 
     if args.rank in ['family', 'subfamily', 'genus', 'species']:
 
         num_bins = 10, #100
         unique_groups = distances['subgroup_name'].unique()
         print(len(unique_groups))
-        df = agg_bin_groups(distances, num_bins=num_bins)
+        df = agg_bin_subgroups(distances, num_bins=num_bins)
 
         fig_file = os.path.join(args.save_path, f'{args.rank}_distributions.pdf')
         plot_box_bin(df, fig_file,
@@ -170,7 +162,44 @@ def main(args):
                      tick_font_size=18,
                      _plt=args.create_plots)
 
-    else:  # class, order
+    elif args.rank in ["class", "order"]:
+
+        # Order subgroups
+        rank_dict = ranked_data[args.rank]
+        # Sort the subgroup names within that rank by descending SDI value.
+        sorted_group_names = sorted(rank_dict.keys(), key=lambda g: rank_dict[g]['sdi'], reverse=True)
+        # Re-sorts the same group names alphabetically.
+        sorted_groups_alphabetically = sorted(sorted_group_names)  # or names.sort(reverse=True)
+
+        # Load metadata file
+        metadata = load_from_pandas(args.metadata_file, load_file=True)
+
+        # Define a list of 10 colors
+        color_palette = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+                         "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",]
+
+        # Get unique classes from the 'class' column
+        unique_classes = metadata['class'].dropna().unique()
+
+        # Create a dictionary mapping each class to a unique color
+        class_color_map = {class_name: color_palette[i] for i, class_name in enumerate(unique_classes)}
+
+        # Filter the DataFrame to exclude rows with NaN in 'class' or 'order'
+        filtered_df = metadata.dropna(subset=['class', 'order'])
+
+        # Map colors to each order based on its class
+        order_color_map = {order: class_color_map[class_name]
+                           for order, class_name in zip(filtered_df['order'], filtered_df['class'])}
+
+        # Map order to their corresponding classes
+        order_class_map = {order: class_name for order, class_name in zip(filtered_df['order'], filtered_df['class'])}
+        print(order_class_map)
+
+        if args.rank == 'class':
+            color_dict = class_color_map
+        else:
+            color_dict = order_color_map
+
         unique_groups = distances['subgroup_name'].unique()
         print(len(unique_groups))
         fig_file = os.path.join(args.save_path, f'{args.rank}_distributions.pdf')
@@ -184,6 +213,8 @@ def main(args):
                  y_label='Damerau-Levenshtein',
                  tick_font_size=24,
                  _plt=args.create_plots)
+    else:
+        raise ValueError(f'Invalid rank {args.rank}')
 
 
 
@@ -221,8 +252,8 @@ if __name__ == '__main__':
                         action='store_true',
                         help='IF plot')
 
-    args = parser.parse_args()
-    main(args)
+    configs = parser.parse_args()
+    main(configs)
 
 
 
